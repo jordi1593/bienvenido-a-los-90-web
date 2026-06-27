908ed2842291e9315d15cb8743bdd0f6ca38cc6c
jordi1593 <bienvenidoalosnoventa@gmail.com>
2026-06-27 17:34:14 +0200

    Revertir color del subtitulo a rosa y aumentar separacion de letras para mejorar legibilidad

--- build-episodes.mjs
+++ build-episodes.mjs
@@ -248,7 +248,7 @@ function episodePage(ep, { prev, next, related, series }) {
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link href="https://fonts.googleapis.com/css2?family=Anton&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
-<link rel="stylesheet" href="../styles.css?v=33" />
+<link rel="stylesheet" href="../styles.css?v=34" />
 
 <meta property="og:type" content="article" />
 <meta property="og:title" content="${escapeHtml(ep.title)}" />
